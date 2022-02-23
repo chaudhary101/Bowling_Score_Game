9d23efb1c10f7327fc3cea5b7ca32d4671e63c2a
Hemant Chaudhary <hemant.chaudhary98@gmail.com>
2022-02-23 15:33:57 +0530

    Code changes for encryption and decryption.

---
+++ FileConvertMachanismUtility/Program.cs
@@ -0,0 +1,61 @@
+﻿using FileConverterUtility;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace FileConvertMachanismUtility
+{
+    class Program
+    {
+        private static string originalFile = @"C:\FileCoversion\AddressProof1.rar";
+        private static string encryptedFile = "AddressProof1.enc"; 
+        //private static string decrFolder = @"C:\FileCoversion\Decrypt\";
+
+        static void Main(string[] args)
+        { 
+            var hashValue = Coverter.GetMD5HashFromFile(originalFile);
+            Console.WriteLine("Hash Value :" + hashValue);
+
+            // Get the certifcate to use to encrypt the key.
+            X509Certificate2 certPublic = Coverter.GetCertificateFromStore("localhost");
+            if (certPublic == null)
+            {
+                Console.WriteLine("Certificate 'CN=CERT_SIGN_TEST_CERT' not found.");
+                Console.ReadLine();
+            }
+             
+            Console.WriteLine("Encoding Value :" + hashValue);
+
+            var encryptedData = Coverter.EncryptData(certPublic.PublicKey.Key.ToXmlString(false), hashValue);
+
+            Console.WriteLine("EncryptData Value :" + encryptedData); 
+
+            X509Certificate2 certPrivteKey = Coverter.GetCertificateFromStore("localhost");
+            if (certPrivteKey == null)
+            {
+                Console.WriteLine("Certificate 'CN=CERT_SIGN_TEST_CERT' not found.");
+                Console.ReadLine();
+            }
+
+            var decryptedData = Coverter.DecryptData(certPrivteKey.PrivateKey.ToXmlString(true), encryptedData);
+
+            Console.WriteLine("DecryptData Value :" + decryptedData);
+
+            Console.WriteLine();
+
+
+            Console.WriteLine("Started the encryption..."); 
+
+            // Encrypt the file using the public key from the certificate.
+            Coverter.EncryptFile(originalFile, (RSA)certPublic.PublicKey.Key);
+             
+            // Decrypt the file using the private key from the certificate.
+            Coverter.DecryptFile(encryptedFile, certPrivteKey.GetRSAPrivateKey());
+
+            Console.WriteLine("Press the Enter key to exit.");
+            Console.ReadLine();
+        } 
+    }
+}
---
+++ InterviewPreparationTest/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace InterviewPreparationTest
+{
+    class GFG
+    {
+
+        // readonly variables
+        public readonly int myvar1;
+        public readonly int myvar2;
+
+        // Values of the readonly
+        // variables are assigned
+        // Using constructor
+        public GFG(int b, int c)
+        {
+
+            myvar1 = b;
+            myvar2 = c;
+            Console.WriteLine("Display value of myvar1 {0}, " +
+                            "and myvar2 {1}", myvar1, myvar2);
+        }
+
+        // Main method
+        static public void Main()
+        {
+            GFG obj1 = new GFG(100, 200);
+        }
+    }
+
+    //public class Program
+    //{
+    //    public static readonly int a; 
+    //    static void Main(string[] args)
+    //    {
+    //        var isNumeric = int.TryParse("123", out int n);
+
+    //        object a = 10;
+    //        object b = 20;
+    //        object c = 2 + 9000000000000;
+    //        Console.WriteLine(c);
+
+    //        string s1 = "asdf";
+    //        string s2 = s1;
+    //        s1 = s1 + "as";
+    //        Console.WriteLine(isNumeric);
+    //        getdate(25,0);
+
+    //    }
+
+    //    public static void getdate(int i , int j)
+    //    {
+    //        try
+    //        {
+    //            a = i;
+
+    //            int t = i / j;
+    //            Console.WriteLine(t);
+    //        }
+    //        catch (Exception ex)
+    //        {
+
+    //            throw;
+    //        }
+           
+
+             
+    //    }
+    //}
+}
